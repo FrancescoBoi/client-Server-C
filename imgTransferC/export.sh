bcaeec0f9c2b430899c0f26c74de5cba93aa8197
Francesco <boi.francesco@gmail.com>
2020-04-27 16:30:51 +0200

    Cleaning2

--- imgTransferC/export.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=/home/fra/Documents/openCV/openCV/build/lib/:/home/fra/Documents/openCV/poco/instDir/lib/:/home/fra/Documents/openCV/SDL2-2.0.8/instDir/lib
-echo $LD_LIBRARY_PATH
